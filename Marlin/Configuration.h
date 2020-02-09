f5cf545deb16d75acd81d58dfac462374513ff25
Marcel Savegnago <marcel.savegnago@gmail.com>
2020-02-09 20:50:08 -0300

    Agora sim

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -723,14 +723,14 @@
    Override with M92
                                         X, Y, Z, E0 [, E1[, E2...]]
 */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 7300, 160 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 8000, 160 }
 
 /**
    Default Max Feed Rate (mm/s)
    Override with M203
                                         X, Y, Z, E0 [, E1[, E2...]]
 */
-#define DEFAULT_MAX_FEEDRATE          { 200, 200, 4, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 200, 200, 3, 25 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
